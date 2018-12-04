d7d41bc53a905ed9823e50b56e29ebf393bd0971
Zach Rigotti <zdrigotti@gmail.com>
2018-12-03 17:56:03 -0800

    Updating server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-var server = app.listen(process.env.PORT || 8080, function () {
-	var port = server.address().port;
-	console.log("App now running on port", port);
-});
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log("App now running on port", port);
