da44e97ed5a58722bbe25b9d9af2726afd9a3480
Zach Rigotti <zdrigotti@gmail.com>
2018-12-04 22:04:43 -0800

    Support for Postgres

--- test.js
+++ test.js
@@ -4,13 +4,12 @@ const test = require('tape');
 
 const child = spawn('node', ['server.js']);
 test('responds to requests', (t) => {
-  t.plan(3);
+  t.plan(2);
   child.stdout.on('data', () => {
-    request('http://127.0.0.1:5000', (error, response, body) => {
+    request('http://127.0.0.1:5000', (error, response) => {
       child.kill();
       t.false(error);
       t.equal(response.statusCode, 200);
-      t.notEqual(body.indexOf('Hello World'), -1);
     });
   });
 });
