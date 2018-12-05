cf8a3f9d00cdd96c98da111f8616037e96bec990
Zach Rigotti <zdrigotti@gmail.com>
2018-12-04 21:18:53 -0800

    Update to include eslint and Travis CI

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const express = require("express");
-const path = require("path");
-var bodyParser = require("body-parser");
+const http = require('http');
 
-const app = express();
-app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.json());
+const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(PORT, () => {
+  // eslint-disable-next-line
+  console.log(`Server running on ${PORT}/`);
+});
+
+/* const express = require('express');
+// const path = require('path');
+
+const server = express();
+// server.use(express.static(__dirname + "/public"));
+// server.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
-app.listen(port);
-console.log("App now running on port", port);
+server.listen(port, () => {
+  // eslint-disable-next-line
+  console.log(`Server listening on port ${port}`)
+}); */
